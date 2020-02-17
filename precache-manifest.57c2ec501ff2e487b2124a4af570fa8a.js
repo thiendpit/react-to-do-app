b6b8bca75162be2e503fb4ad15b87ac8202f95ed
Kiều Phước Thiện <thien.dpit@gmail.com>
2020-02-17 08:12:45 +0700

    Updates

---
+++ precache-manifest.57c2ec501ff2e487b2124a4af570fa8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67923d0b5dbcb6e9c8510f8b0c3c744",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "1ed377f1f4763b156c56",
+    "url": "/react-to-do-app/static/css/main.25db90ea.chunk.css"
+  },
+  {
+    "revision": "d31fbc40b4d4e0fad6b5",
+    "url": "/react-to-do-app/static/js/2.69878da4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-to-do-app/static/js/2.69878da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed377f1f4763b156c56",
+    "url": "/react-to-do-app/static/js/main.06a632f7.chunk.js"
+  },
+  {
+    "revision": "cf1b8f386a694d841b04",
+    "url": "/react-to-do-app/static/js/runtime-main.a7e25141.js"
+  }
+]);
